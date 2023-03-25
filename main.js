240ce970607ab596112ce8396ac52fb5284fb454
Cyrus Cheung <cyruscheung@cmail.carleton.ca>
2023-03-25 13:08:38 -0400

    fixed index issue

--- main.js
+++ main.js
@@ -4,8 +4,30 @@ const inputTodo = document.querySelector("#inputTodo");
 const todoList = document.querySelector(".todolist1");
 
 const todos = [];
-
 const doneTodos = [];
+
+let sortable = Sortable.create(todoList, // Element dragging ended
+{onEnd: function (evt) {
+  let itemEl = evt.item;  // dragged HTMLElement
+  evt.to;    // target list
+  evt.from;  // previous list
+  evt.oldIndex;  // element's old index within old parent
+  evt.newIndex;  // element's new index within new parent
+  evt.oldDraggableIndex; // element's old index within old parent, only counting draggable elements
+  evt.newDraggableIndex; // element's new index within new parent, only counting draggable elements
+  evt.clone // the clone element
+  evt.pullMode;  // when item is in another sortable: "clone" if cloning, true if moving
+  console.log(evt); 
+  //reorder array based on old index and new index
+  let temp = todos[evt.newIndex]; //copy of newIndex
+  todos[evt.newIndex] = todos[evt.oldIndex];
+  todos[evt.oldIndex] = temp;
+  console.log(todos);
+  drawToDoList();
+},
+
+}
+);
 
 function drawToDoList() {
 
